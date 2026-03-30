46e77c7410a16e316badd90c46d597757a1a160f
swostika61 <swostikalamichhane61@gmail.com>
2026-03-30 15:10:43 -0400

    improve color contrast

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,21 +1,32 @@
-import { header } from '../../portfolio'
+import { header, about } from '../../portfolio'
 import Navbar from '../Navbar/Navbar'
 import './Header.css'
 
 /**
- * Header component - Main header with site title and navigation
+ * Header component - Stylish header section with introduction
  */
 const Header = () => {
   const { title } = header
+  const { name, role } = about
 
   return (
-    <header id='header' className='header center'>
-      <h3 className='header__brand'>
-        <a href='#header' className='link'>
-          {title}
-        </a>
-      </h3>
-      <Navbar />
+    <header id='header' className='header'>
+      <nav className='header__nav'>
+        <h3 className='header__brand'>
+          <a href='#header' className='link'>
+            {title}
+          </a>
+        </h3>
+        <Navbar />
+      </nav>
+
+      <div className='header__content'>
+        <h1 className='header__title'>{name}</h1>
+        <p className='header__subtitle'>{role}</p>
+        <p className='header__description'>
+          Passionate Front End Engineer crafting beautiful, responsive web experiences
+        </p>
+      </div>
     </header>
   )
 }
